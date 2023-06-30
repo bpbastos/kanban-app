d245c7aa9938d80ac0bc42d6f03bcd93777b6e73
Bruno Bastos <pinheirobastos@gmail.com>
2023-06-30 17:22:43 -0300

    feat: criação do component addtaskform

---
+++ src/components/AddTaskForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="flex flex-col space-y-2">
+        <input maxlength="45" type="text" v-model="newTaskTitle" ref="newTaskTitleInput" @keyup.enter="addNewTask" @keyup.esc="emit('cancel')"
+            class="rounded bg-white appearance-none p-3 text-sm border-2 focus:outline-none focus:bg-white focus:border-secondary"
+            placeholder="Insira um título para tarefa...">
+        <div class="flex gap-1 text-sm">
+            <button @click="addNewTask">
+                <svg class="w-5 h-5 text-white rounded shadow bg-green-400 hover:bg-green-600 active:bg-green-800 active:border-2 border-gray-300"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                </svg>
+            </button>
+            <button @click="emit('cancel')">
+                <svg class="w-5 h-5 text-white rounded shadow bg-red-400 hover:bg-red-600 active:bg-red-800 active:border-2 border-gray-300"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import TaskDataService from '../services/TaskDataService'
+
+const props = defineProps({
+    workflowId: Number,
+    boardId: Number
+})
+
+const emit = defineEmits(['add', 'cancel'])
+
+const newTaskTitle = ref('')
+const newTaskTitleInput = ref(null)
+
+const addNewTask = () => {
+    if (newTaskTitle.value.trim().length) {
+        TaskDataService.add(newTaskTitle.value.trim(), props.boardId, props.workflowId)
+            .then((response) => {
+                emit('add')
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+        newTaskTitle.value = ''
+    }
+}
+</script>
--- src/components/WorkflowCard.vue
+++ src/components/WorkflowCard.vue
@@ -2,25 +2,31 @@
   <div class="h-full w-[265px] rounded-lg pt-1 drop-shadow-xl">
     <section class="flex flex-col rounded-b-lg bg-board drop-shadow-xl">
       <p class="text-secondary text-lg font-medium p-4">{{ name }}</p>
-      <div class="max-h-[568px] overflow-auto scrollbar scrollbar-w-1 scrollbar-thumb-slate-300 m-2 ml-2 mr-2 space-y-2">
-        <TaskCard v-for="task in tasks" :priority="task.priority" :title="task.title" :total-sub-tasks-done="task.subtasks?.filter((item)=>{return item.done})?.length" :total-sub-tasks="task.subtasks?.length" />
+      <div class="max-h-[568px] overflow-auto scrollbar scrollbar-w-1 scrollbar-thumb-slate-300 ml-2 mr-2 space-y-2">
+        <TaskCard v-for="task in tasks" :id="task.id" :priority="task.priority" :title="task.title"
+          :total-sub-tasks-done="task.subtasks?.filter((item) => { return item.done })?.length"
+          :total-sub-tasks="task.subtasks?.length" @delete="updateTasks(boardId, id)" />
+        <AddTaskForm v-show="isAddNewTaskButtonClicked" :workflow-id="id" :board-id="boardId"
+          @cancel="isAddNewTaskButtonClicked = false"
+          @add="updateTasks(boardId, id); isAddNewTaskButtonClicked = false" />
       </div>
-      <RouterLink :to="{ name: 'AddTask', params: { boardId: boardId, workflowId: id } }"
+      <a href="#" @click="showAddNewTaskForm"
         class="flex flex-row p-4 items-center justify-center text-secondary text-base font-medium">
         <i class="fa-solid fa-plus"></i>
         <p class="">Adicionar tarefa</p>
-      </RouterLink>
+      </a>
     </section>
   </div>
 </template>
 
 <script setup>
 import { ref, watch } from 'vue';
-import { RouterLink } from 'vue-router'
-import TaskCard from './TaskCard.vue'
-import TaskDataService from '../services/TaskDataService'
+import TaskCard from '@/components/TaskCard.vue'
+import AddTaskForm from '@/components/AddTaskForm.vue'
+import TaskDataService from '@/services/TaskDataService'
 
 const tasks = ref([])
+const isAddNewTaskButtonClicked = ref(false)
 
 const props = defineProps({
   name: String,
@@ -36,6 +42,11 @@ const updateTasks = async (boardId, workflowId) => {
     .catch((e) => {
       console.log(e)
     })
+}
+
+const showAddNewTaskForm = () => {
+  isAddNewTaskButtonClicked.value = true
+
 }
 
 //created hook
