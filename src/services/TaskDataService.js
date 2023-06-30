1480d7e13c81043b8803bb4b3279318dc9775841
Bruno Bastos <pinheirobastos@gmail.com>
2023-06-30 17:23:56 -0300

    feat: adicição da rotina para inclusão de nova tarefa.

--- src/services/TaskDataService.js
+++ src/services/TaskDataService.js
@@ -2,16 +2,36 @@ import http from "@/http-common";
 
 class TaskDataService {
   getAll() {
-    return http.get("/tasks");
+    return http.get("/tasks")
   }
 
   getByBoardIdAndWorkflowId(boardId, workflowId) {
     return http.get(`/tasks?board_id=${boardId}&workflow_id=${workflowId}`)
   }
 
-  get(id) {
-    return http.get(`/tasks/${id}`);
+  getById(id) {
+    return http.get(`/tasks/${id}`)
   }
+
+  add(title,boardId,workflowId) {
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let currentDate = `${day}-${month}-${year}`;    
+    return http.post("/tasks", {
+      title: title,
+      priority: "none",
+      board_id: boardId,
+      workflow_id: workflowId,
+      date_added: currentDate
+    })
+  }
+
+  delete(id) {
+    return http.delete(`/tasks/${id}`)
+  }
+
 }
 
 export default new TaskDataService();
