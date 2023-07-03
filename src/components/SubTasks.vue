1d64521f92e12778a12355d556c08a2022f2114c
Bruno Bastos <pinheirobastos@gmail.com>
2023-07-03 09:26:49 -0300

    refactor: criação do componente para progresso de tarefas.

--- src/components/SubTasks.vue
+++ src/components/SubTasks.vue
@@ -4,32 +4,7 @@
       <span class="text-base label-text uppercase font-normal">sub tarefas:</span>
     </label>
     <div>
-      <div class="flex justify-stretch items-center space-x-1 m-2" v-show="totalSubTasks > 0">
-        <div
-          class="badge font-semibold"
-          :class="totalSubTasks == totalSubTasksDone ? 'badge-success' : 'badge-info'"
-        >
-          <svg
-            class="fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="19"
-          >
-            <path fill="none" d="M0 0h24v24H0z"></path>
-            <path
-              d="M4 3h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm1 2v14h14V5H5zm6.003 11L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z"
-            ></path>
-          </svg>
-          <p class="tracking-wider">{{ totalSubTasksDone }}/{{ totalSubTasks }}</p>
-        </div>
-
-        <progress
-          class="progress shadow-2xl"
-          :class="totalSubTasks == totalSubTasksDone ? 'progress-success' : 'progress-info'"
-          :value="totalSubTasksDone"
-          :max="totalSubTasks"
-        ></progress>
-      </div>
+      <TaskProgressBar :total-tasks="totalSubTasks" :total-tasks-done="totalSubTasksDone" />
       <ul>
         <li
           v-for="(st, index) in subTasks"
@@ -72,6 +47,7 @@
 
 <script setup>
 import { ref } from 'vue'
+import TaskProgressBar from './TaskProgressBar.vue';
 
 const props = defineProps({
   subTasks: Array
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -26,27 +26,7 @@
     </div>
     <div @click="goToEditTask">
       <p class="card-actions text-base text-base-content m-2">{{ title }}</p>
-      <div class="card-actions justify-between items-center" v-show="totalSubTasks > 0">
-        <div class="badge font-semibold" :class="totalSubTasks==totalSubTasksDone?'badge-success':'badge-info'">
-          <svg
-            class="fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="19"
-          >
-            <path fill="none" d="M0 0h24v24H0z"></path>
-            <path
-              d="M4 3h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm1 2v14h14V5H5zm6.003 11L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z"
-            ></path>
-          </svg>
-          <p class="tracking-wider">{{ totalSubTasksDone }}/{{ totalSubTasks }}</p>
-        </div>
-        <progress
-          class="progress shadow-2xl w-4/6" :class="totalSubTasks==totalSubTasksDone?'progress-success':'progress-info'"
-          :value="totalSubTasksDone"
-          :max="totalSubTasks"
-        ></progress>
-      </div>
+      <TaskProgressBar :total-tasks="totalSubTasks" :total-tasks-done="totalSubTasksDone" />
     </div>
   </div>
   <DeleteConfirmationModal
@@ -64,6 +44,7 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import DeleteConfirmationModal from './DeleteConfirmationModal.vue'
 import TaskDataService from '../services/TaskDataService'
+import TaskProgressBar from './TaskProgressBar.vue';
 
 const emit = defineEmits(['delete'])
 const router = useRouter()
