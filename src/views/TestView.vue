a8eca411352029bb505cb3d8797a79515240a5bd
Bruno Bastos <pinheirobastos@gmail.com>
2023-07-14 22:51:37 -0300

    fix: testes com composables reativas

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,24 +1,34 @@
 <template>
-    <WorkflowCard
-      v-if="workflows.id"
-      :workflow="workflows"
-      :board-id="boardId"
-      :key="upd"
-    />  
+    User ID<input type="text" v-model="userId"> <strong v-if="user">{{ user.first_name }}</strong>
+    Task ID<input type="text" v-model="taskId"> <strong v-if="task">{{ task.title  }}</strong>
+    Priority ID<input type="text" v-model="priorityId"> <strong v-if="priority">{{ priority.name }}</strong>
+    Board ID<input type="text" v-model="boardId"> <strong v-if="board">{{ board.name }}</strong>
+    Workflow ID<input type="text" v-model="workflowId"> <strong v-if="workflow">{{ workflow.name }}</strong>
+   
+    <!--WorkflowCard
+      :workflow="workflow"
+      :board-id="parseInt(boardId)"
+    /-->  
 </template>
 <script setup>
 import { ref } from 'vue';
-import { useFetchWorkflows } from '@/composables/WorkflowData'
 import WorkflowCard from '@/components/WorkflowCard.vue';
+import { useFetchPriorities } from '../composables/PriorityData';
+import { useFetchBoards } from '../composables/BoardData';
+import { useFetchTasks } from '../composables/TaskData';
+import { useFetchWorkflows } from '@/composables/WorkflowData'
+import { useFetchUsers } from '../composables/UserData';
 
+const userId = ref('yoda')
+const taskId = ref(1)
+const priorityId = ref(1)
 const boardId = ref(1)
-const upd = ref(0)
-const { workflows } =  useFetchWorkflows()
+const workflowId = ref(1)
 
-
-const updateWorkflow = () => {
-    upd.value++
-    console.log(upd.value)
-}
+const { users: user } =  useFetchUsers(userId)
+const { tasks: task } =  useFetchTasks(taskId)
+const { priorities: priority } =  useFetchPriorities(priorityId)
+const { boards: board } =  useFetchBoards(boardId)
+const { workflows: workflow } =  useFetchWorkflows(workflowId)
 
 </script>
