7b12ad92cfb85f6a9218a6e982af0a39ccfec7a1
Bruno Bastos <pinheirobastos@gmail.com>
2023-07-14 22:48:10 -0300

    fix: conversão das props em variaveis reativas.

--- src/components/WorkflowCard.vue
+++ src/components/WorkflowCard.vue
@@ -3,7 +3,7 @@
     <div class="card bg-base-200 overflow-auto scrollbar scrollbar-w-1 scrollbar-thumb-primary">
       <p class="card-title text-base-content text-lg font-medium m-3">{{ workflow.name }}</p>
       <div class="card-actions justify-center ml-2 mr-2">
-        <TaskCard v-for="task in tasks" :task="task" @deleted="updateTasks"/>
+        <TaskCard v-for="task in tasks" :task="task" @deleted="updateTasks" :key="task.id"/>
         <AddTaskForm v-show="isAddNewTaskButtonClicked" :workflow-id="workflow.id" :board-id="boardId" @added="updateTasks" @canceled="isAddNewTaskButtonClicked = false" />
       </div>
       <div class="card-actions m-3">
@@ -21,29 +21,38 @@
 </template>
 
 <script setup>
-import { ref, watchEffect } from 'vue'
+import { computed, ref, toRefs, watchEffect } from 'vue'
 import TaskCard from '@/components/TaskCard.vue'
 import AddTaskForm from '@/components/AddTaskForm.vue'
 import { useFetchTasks } from '@/composables/TaskData'
-
-const isAddNewTaskButtonClicked = ref(false)
-const { tasks, isReady, fetch } = useFetchTasks(0, props.boardId, props.workflow.id)
 
 const props = defineProps({
   workflow: {
     type: Object,
     required: true
   },
-  boardId: Number
+  boardId: {
+    type: Number,
+    required: true
+  }
 })
 
+const isAddNewTaskButtonClicked = ref(false)
+
+//Converts props to ref to keep reactivity
+const { boardId } = toRefs(props)
+const workflowId = computed(()=>props.workflow.id)
+
+//Fetch the data and keeps updated 
+const { tasks, isReady, fetch } = useFetchTasks(0, boardId, workflowId)
+
 const updateTasks = () => {
-  fetch(0, props.boardId, props.workflow.id)
+  fetch(0, boardId.value, workflowId.value)
   watchEffect(() => {
     if (isReady.value) {
       isAddNewTaskButtonClicked.value = false
     }
-  })  
-  
+  })
 }
+
 </script>
