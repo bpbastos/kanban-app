1a35c637784e125028d265097f3b71132d7e82c0
Bruno Bastos <pinheirobastos@gmail.com>
2023-07-16 12:29:18 -0300

    feat: add data de criação as notificações.

--- src/stores/notification.js
+++ src/stores/notification.js
@@ -1,39 +1,49 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 
 export const useNotificationStore = defineStore('notification', () => {
   //Store the notifications on localStorare
   const messages = useLocalStorage('notifications', [])
-
+  
   function info(m) {
+    const d = new Date()
     const tempMsg = {
-        type: 'info',
-        text: m
-    }
+      type: 'info',
+      text: m,
+      date: d.toISOString()
+    }     
     messages.value.unshift(tempMsg)
   }
 
   function success(m) {
+    const d = new Date()    
     const tempMsg = {
-        type: 'success',
-        text: m
-    }
+      type: 'info',
+      text: m,
+      date: d.toISOString()
+    }   
+    console.log(currentPath.value)
     messages.value.unshift(tempMsg)
   }
 
   function warning(m) {
+    const d = new Date()    
     const tempMsg = {
-        type: 'warning',
-        text: m
-    }
+      type: 'info',
+      text: m,
+      date: d.toISOString()
+    }   
     messages.value.unshift(tempMsg)
   }  
 
   function error(m) {
+    const d = new Date()    
     const tempMsg = {
-        type: 'error',
-        text: m
-    }
+      type: 'info',
+      text: m,
+      date: d.toISOString()
+    }   
     messages.value.unshift(tempMsg)
   }  
 
