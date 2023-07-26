2792400e9f396a44ac10df4417af241faf486a04
Bruno Bastos <pinheirobastos@gmail.com>
2023-07-26 17:58:24 -0300

    refactor: removendo trycatch block. Deixando o parse server curidar das exceções

--- backend/cloud/functions.js
+++ backend/cloud/functions.js
@@ -3,66 +3,41 @@ import { fromGlobalId, toGlobalId } from 'graphql-relay';
 Parse.Cloud.define('removeTask', async req => {
   const { user, params: { taskId } } = req;
   const { id:taskObjectId } = fromGlobalId(taskId); 
+  const queryTask = new Parse.Query('Task');
+  queryTask.include("workflow")
+  const task = await queryTask.get(taskObjectId); 
 
-  try {
-    const queryTask = new Parse.Query('Task');
-    queryTask.include("workflow")
-    const task = await queryTask.get(taskObjectId); 
+  const workflow = task.get("workflow");
+  await workflow.remove("tasks", task.id)
 
-    const workflow = task.get("workflow");
-    await workflow.remove("tasks", task.id)
+  const removedTask = await task.destroy()
 
-    const removedTask = await task.destroy()
-
-    return { ...removedTask.toJSON(), id: taskId };
-
-  } catch (error) {
-    req.log.error(`removeTask: ${error.message}`)
-    throw new Parse.Error(Parse.Error.OTHER_CAUSE, "Não foi possível remover a tarefa.");
-  }
+  return { ...removedTask.toJSON(), id: taskId };
 },
 {
-  fields: ["taskId"],
-  requireUser: true
+  requireUser: true,
+  fields: ["taskId"]
 })
 
 Parse.Cloud.define('addTask', async req => {
   const { user, params: { title, workflowId, boardId  } } = req;
-  let board,workflow,priority;
-
   //Validations
-  if (title.trim().length < 5 || title.trim().length > 25)  {
-    throw new Parse.Error(Parse.Error.VALIDATION_ERROR, "Tamanho do título invalido - < 5 ou > 25.");
-  }  
 
-  try {
-    // Decode the incoming Relay Node Id to a
-    // Parse objectId for Cloud Code use.    
-    const { id: boardObjectId } = fromGlobalId(boardId); 
-    const queryBoard = new Parse.Query('Board');
-    board = await queryBoard.get(boardObjectId);  
-  } catch (error) {
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, "Board ID informado não encontrado.");
-  }
+  // Decode the incoming Relay Node Id to a
+  // Parse objectId for Cloud Code use.    
+  const { id: boardObjectId } = fromGlobalId(boardId); 
+  const { id: workflowObjectId } = fromGlobalId(workflowId);
 
-  try {
-    // Decode the incoming Relay Node Id to a
-    // Parse objectId for Cloud Code use.
-    const { id: workflowObjectId } = fromGlobalId(workflowId);
-    const queryWorkflow = new Parse.Query('Workflow');
-    workflow = await queryWorkflow.get(workflowObjectId);  
-  } catch (error) {
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, "Workflows ID informado não encontrado.");
-  }
+  const queryBoard = new Parse.Query('Board');
+  const board = await queryBoard.get(boardObjectId);  
 
-  try {
-    const queryPriority = new Parse.Query('Priority');    
-    queryPriority.equalTo("name", "Baixa");    
-    priority = await queryPriority.first();
-  } catch (error) {
-    throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, "Prioridade 'Baixa' não encontrada.");
-  }
- 
+  const queryWorkflow = new Parse.Query('Workflow');
+  const workflow = await queryWorkflow.get(workflowObjectId);  
+
+  const queryPriority = new Parse.Query('Priority');    
+  queryPriority.equalTo("name", "Baixa");    
+  const priority = await queryPriority.first();
+
   const newTask = new Parse.Object("Task");
   newTask.set("board", board);
   newTask.set("workflow", workflow);
@@ -80,7 +55,6 @@ Parse.Cloud.define('addTask', async req => {
   // Convert to a JSON object to handle adding the
   // Relay Node Id property.
   return { ...savedTask.toJSON(), id: taskId };
-
 },
 {
   requireUser: true,
@@ -92,6 +66,14 @@ Parse.Cloud.define('addTask', async req => {
         return val.trim().length >= 5 && val.trim().length <= 25;
       },
       error: 'Tamanho do título invalido - < 5 ou > 25.'
+    },
+    workflowId: {
+      required: true,
+      type: String,
+    },
+    boardId: {
+      required: true,
+      type: String,
     }
   },  
 }
